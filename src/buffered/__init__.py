b28c46d841ac2fefdef4fc75e42ed82862e5d9b8
Matthew Davidson <mjdavidson@pm.me>
2024-02-02 19:55:08 -0800

    first commit

---
+++ src/buffered/__init__.py
@@ -0,0 +1,14 @@
+version = "0.0.1"
+
+from buffered.buffer import (
+    Buffer,
+    MetricsBuffer,
+    PacketBuffer,
+    PackagedBuffer,
+)
+from buffered.packager import (
+    Packager,
+    SeparatorPackager,
+    PicklerPackager,
+    JSONPackager,
+)
